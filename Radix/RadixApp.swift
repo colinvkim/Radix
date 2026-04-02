5e66acc5b852d366105d271458a9d618dd390cab
Colin Kim <121141519+colinvkim@users.noreply.github.com>
2026-04-01 21:30:14 -0700

    Initial Commit

---
+++ Radix/RadixApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadixApp.swift
+//  Radix
+//
+//  Created by Colin Kim on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct RadixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
