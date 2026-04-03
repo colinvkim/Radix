c083fcd6e8ef44f1e978d5042b481546f26d9303
Colin Kim <121141519+colinvkim@users.noreply.github.com>
2026-04-03 16:19:08 -0700

    feat!: add auto update with sparkle

--- Radix/RadixApp.swift
+++ Radix/RadixApp.swift
@@ -6,14 +6,52 @@
 //
 
 import AppKit
+import Combine
+import Sparkle
 import SwiftUI
+
+// This view model class publishes when new updates can be checked by the user
+final class CheckForUpdatesViewModel: ObservableObject {
+    @Published var canCheckForUpdates = false
+
+    init(updater: SPUUpdater) {
+        updater.publisher(for: \.canCheckForUpdates)
+            .assign(to: &$canCheckForUpdates)
+    }
+}
+
+// This is the view for the Check for Updates menu item
+// Note this intermediate view is necessary for the disabled state on the menu item to work properly before Monterey.
+// See https://stackoverflow.com/questions/68553092/menu-not-updating-swiftui-bug for more info
+struct CheckForUpdatesView: View {
+    @ObservedObject private var checkForUpdatesViewModel: CheckForUpdatesViewModel
+    private let updater: SPUUpdater
+    
+    init(updater: SPUUpdater) {
+        self.updater = updater
+        
+        // Create our view model for our CheckForUpdatesView
+        self.checkForUpdatesViewModel = CheckForUpdatesViewModel(updater: updater)
+    }
+    
+    var body: some View {
+        Button("Check for Updates…", action: updater.checkForUpdates)
+            .disabled(!checkForUpdatesViewModel.canCheckForUpdates)
+    }
+}
 
 @main
 struct RadixApp: App {
     @StateObject private var appModel = AppModel()
+    private let updaterController: SPUStandardUpdaterController
 
     init() {
         NSWindow.allowsAutomaticWindowTabbing = false
+        updaterController = SPUStandardUpdaterController(
+            startingUpdater: true,
+            updaterDelegate: nil,
+            userDriverDelegate: nil
+        )
     }
 
     var body: some Scene {
@@ -24,7 +62,13 @@ struct RadixApp: App {
         }
         .defaultSize(width: 1480, height: 920)
         .windowResizability(.contentMinSize)
-        .commands { RadixCommands(appModel: appModel) }
+        .commands {
+            RadixCommands(appModel: appModel)
+
+            CommandGroup(after: .appInfo) {
+                CheckForUpdatesView(updater: updaterController.updater)
+            }
+        }
 
         Settings {
             SettingsView()
